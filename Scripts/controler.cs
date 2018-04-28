269ab5c9d0400d2a5d4a714d88769a046bcd934e
gavlasj <filipjegeno@gmail.com>
2018-04-28 20:49:34 +0200

    Add files via upload

---
+++ Scripts/controler.cs
@@ -0,0 +1,54 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class controler : MonoBehaviour {
+
+    public plant plant;
+    public float maxX;
+    public float y;
+    public float maxZ;
+
+    public float treeDensity;
+    private float baseDensity = 12.0f;
+
+
+
+    public bool ShouldPlaceTree(float chance)
+    {
+        if (Random.Range(0.0f, 1.0f) <= chance)
+        {
+            return true;
+        }
+        return false;
+    }
+    // Use this for initialization
+    void Start () {
+
+        for (int i = 2; i < maxX; i++)
+        {
+            for (int j = 2; j < maxZ; j++)
+            {
+                float chance = Mathf.PerlinNoise(i, j) / (baseDensity / treeDensity);
+
+                if (ShouldPlaceTree(chance))
+                {
+                    plant.BuildNew(i, y, j);
+                }
+            }
+        }
+        
+    }
+
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (Input.GetKeyDown("space"))
+        {
+            plant.waveOn = !plant.waveOn;
+        }
+    }
+
+    
+}
---
+++ Scripts/movement.cs
@@ -0,0 +1,33 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class movement : MonoBehaviour {
+
+	// Use this for initialization
+	void Start () {
+		
+	}
+
+    public float speedH = 2.0f;
+    public float speedV = 2.0f;
+
+    private float yaw = 0.0f;
+    private float pitch = 0.0f;
+
+    void Update ()
+    {
+        float moveHorizontal = Input.GetAxis("Horizontal");
+        float moveVertical = Input.GetAxis("Vertical");
+
+        if (Camera.current != null)
+        {
+            Camera.current.transform.Translate(new Vector3(moveHorizontal, 0.0f, moveVertical));
+        }
+        yaw += speedH * Input.GetAxis("Mouse X");
+        pitch -= speedV * Input.GetAxis("Mouse Y");
+
+        transform.eulerAngles = new Vector3(pitch, yaw, 0.0f);
+
+    }
+}
